29481fe6b899a6834d654c314fac6e8b6661bfac
Ivan Tabarelli <ivan.tabarelli@sasabz.it>
2022-01-21 10:23:14 +0100

    symlinks and aliases.

---
+++ oh-my-zhs/custom/aliases.zsh
@@ -0,0 +1,9 @@
+
+alias ll="colorls -alGh --gs"
+alias l="colorls -1"
+alias python="python3"
+alias pip="pip3"
+alias wo="cd ~/Workspace"
+alias flutter="/Users/ivan/Workspace/flutter/bin/flutter"
+alias cat="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
+alias vim="nvim"
---
+++ symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ln -s /path/to/original /path/to/link
+
+ln -s ~/dotfiles/oh-my-zsh/custom/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+ln -s ~/dotfiles/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/ttmux/tmux.conf ~/.tmux.conf
+
