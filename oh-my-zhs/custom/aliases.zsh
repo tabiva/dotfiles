3ed80703545116806d63632333ee324ac9ce0ae7
ivan.tabarelli <ivan.tabarelli@protonmail.com>
2022-01-24 20:23:32 +0100

    Zsh configs.

--- oh-my-zhs/custom/aliases.zsh
+++ oh-my-zhs/custom/aliases.zsh
@@ -1,9 +1,11 @@
 
 alias ll="colorls -alGh --gs"
 alias l="colorls -1"
-alias python="python3"
+# alias python="python3"
+alias python=/usr/local/bin/python3
 alias pip="pip3"
 alias wo="cd ~/Workspace"
 alias flutter="/Users/ivan/Workspace/flutter/bin/flutter"
 alias cat="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
 alias vim="nvim"
+alias big="tmuxinator thebig"
--- symlinks.sh
+++ symlinks.sh
@@ -2,8 +2,8 @@
 
 # ln -s /path/to/original /path/to/link
 
-ln -s ~/dotfiles/oh-my-zsh/custom/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-ln -s ~/dotfiles/alacritty ~/.config/alacritty
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/ttmux/tmux.conf ~/.tmux.conf
+# ln -s ~/dotfiles/oh-my-zsh/custom/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+# ln -s ~/dotfiles/alacritty ~/.config/alacritty
+# ln -s ~/dotfiles/nvim ~/.config/nvim
+# ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
